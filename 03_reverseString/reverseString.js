b047a4df1a2ecc75818e7705585a974b4a88d434
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-06-19 18:46:00 -0700

    Finished JS TDD Lesson 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newStr = ""
+    let newStrLen = string.length - 1;
+    for (let i = newStrLen; i >= 0; i--) {
+        newStr += string[i];
+    }
+    return newStr;
 };
 
 // Do not edit below this line
