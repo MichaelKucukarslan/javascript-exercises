8b304a4cfa544421cae62f2c0baeae1e498e7f01
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-06-21 14:05:16 -0700

    Finished JS TDD Lesson 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
 const sumAll = function() {
-
+    let smallNumber = arguments[0];
+    let largeNumber = arguments[1];
+    //Tests if both arguments are positive numbers
+    if (smallNumber < 0 || largeNumber < 0) {
+        return "ERROR";
+    }
+    // Tests is both arguments are numbers
+    if (typeof(smallNumber) != typeof(1) || typeof(largeNumber) != typeof(1)) {
+        return "ERROR";
+    }
+    // Tests if the arguments are in the correct order
+    // If they are not they get switched
+    if (smallNumber > largeNumber){
+        let temp = smallNumber;
+        smallNumber = largeNumber;
+        largeNumber = temp;
+    }
+    let sum = 0;
+    for(let i = smallNumber; i <= largeNumber; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
