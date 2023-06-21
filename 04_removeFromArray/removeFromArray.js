781adf0628bcd81cf16f99375a53d81bfad4121b
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-06-20 19:48:27 -0700

    Finished JS TDD 04
    I finish the leason in my own way.
    The solution they used adds all values that are not the the exclusion
    list where mine removes form the original array from the exclusion list

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+    let originalArray = arguments[0];
+    let originalArrayLength = originalArray.length;
+    let argumentsLength = arguments.length;
+    for (let i = originalArrayLength; i >= 0; i--) {
+        for (let j = argumentsLength - 1; j >= 1; j--){
+            console.log(originalArray[i] + " " + arguments[j]);
+            if (originalArray[i] === arguments[j]){
+                originalArray.splice(i, 1);
+            }
+            console.log("Argument J: " + arguments[j]);
+        }
+    }
+    return originalArray;
 };
 
 // Do not edit below this line
