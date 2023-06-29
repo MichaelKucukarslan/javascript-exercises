47b9ce197116a3c85426e87047efa5da8ab0c92c
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-06-28 18:14:33 -0700

    Finished JS TDD Leason 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibNum) {
+    fibNum = parseInt(fibNum, 10);
+    let num1 = 0;
+    let num2 = 1;
+    let cur = num1 + num2;
+    if (fibNum < 0){
+        return "OOPS";
+    }
+    if (fibNum == 1 || fibNum ==2){
+    return 1;
+    }
+    for (let i = 3; i <= fibNum; i++){
+        num1 = num2;
+        num2 = cur;
+        cur = num1 + num2;
+}
+    return cur;
 };
 
 // Do not edit below this line
