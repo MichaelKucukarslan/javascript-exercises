b7543c1bea586dca3ac678c6a8268afe99204cf6
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-06-28 17:35:06 -0700

    Finished JS TDD Leason 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(...args) {
+  let total = 0;
+  args.forEach(element => {
+    total += element;
+  });
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	let total = args[0];
+  let length = args.length;
+  for (let i = length - 1; i > 0; i--){
+    total -= args[i];
+  }
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  array.forEach(element => {
+    total += element;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let total = 1;
+  let length = args.length;
+  for (let i = 0; i <  length; i++) {
+    total *= args[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(...args) {
+	let total = args[0];
+  total = total ** args[1];
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  if (num == 0){
+    return 1;
+  }
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
