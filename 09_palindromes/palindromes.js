8b4181298bf0c3c93f0e6a8185a0d1ea9bd53ab4
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-06-28 17:53:56 -0700

    Finished JS TDD Leason 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (letters) {
+    // 00583
+    let lettersToCheck = letters.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return lettersToCheck.split("").reverse().join("") == lettersToCheck;
 };
 
 // Do not edit below this line
